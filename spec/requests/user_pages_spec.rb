d3de4dc1b8b9e287bf67fb2f3e59fb2647ba08f8
ideodora <ideodora@gmail.com>
2012-11-27 15:56:14 +0900

    till 11.2.3

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -105,6 +105,11 @@ describe "User pages" do
 			it { should have_content(user.microposts.count) }
 		end
 
+		describe "follow/unfollow buttons" do
+			let(:other_user) { FactoryGirl.create(:user) }
+			before { sign_in user }
+
+
 	end
 
 	describe "edit" do
@@ -145,5 +150,34 @@ describe "User pages" do
 		end
 
 	end
+	
+	describe "following/followers" do
+		let(:user) { FactoryGirl.create(:user) }
+		let(:other_user) { FactoryGirl.create(:user) }
+		before { user.follow!(other_user) }
+
+		describe "followed users" do
+			before do
+				sign_in user
+				visit following_user_path(user)
+			end
+			
+			it { should have_selector('title', text: full_title('Following')) }
+			it { should have_selector('h3', text: 'Following') }
+			it { should have_link(other_user.name, href: user_path(other_user)) }
+		end
+
+		describe "followers" do
+			before do
+				sign_in other_user
+				visit following_user_path(other_user)
+			end
+			
+			it { should have_selector('title', text: full_title('Followers')) }
+			it { should have_selector('h3', text: 'Followers') }
+			it { should have_link(user.name, href: user_path(user)) }
+		end
+	end
+		
 end
 
